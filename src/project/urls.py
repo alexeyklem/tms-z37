c0e98eb259c3cdf916a8ecbcceb3e96ba407e797
alexeyklem <KlepaN567@gmail.com>
2020-11-20 20:12:51 +0300

    updated index page

--- src/project/urls.py
+++ src/project/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
+from django.http import HttpResponse
+
+
+def index(_request: HttpRequest) -> HttpResponse:
+    return HttpResponse("xxx")
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
 ]
